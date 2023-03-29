c4f52d8f060141b133b62abfae01176b8643a72c
Ronald <nottinsonstephen@gmail.com>
2023-03-29 09:07:58 -0700

    Printf Prototypes and handlers

--- main.h
+++ main.h
@@ -45,6 +45,5 @@ int _strlenc(const char *s);
 int printf_37(void);
 int printf_char(va_list val);
 int printf_string(va_list val);
-int get_width(const char *format, int *i, va_list list);
 
 #endif
